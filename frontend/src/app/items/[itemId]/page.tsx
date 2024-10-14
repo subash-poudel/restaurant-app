92c4b77609287bdd6ae2fd31cab1771c0da19b10
Subash Poudel <blogsubash@gmail.com>
2024-10-14 13:09:25 +0545

    main: Make whole website static

--- frontend/src/app/items/[itemId]/page.tsx
+++ frontend/src/app/items/[itemId]/page.tsx
@@ -1,47 +1,54 @@
 import { MENU_ITEMS } from "@/app/menu";
 import { MenuItem } from "@/app/models";
-import Link from "next/link";
+// import { GetStaticPaths, GetStaticProps } from "next";
 
-function MenuItemList({
-  items,
-  category,
-}: {
-  items: MenuItem[];
-  category: string;
-}) {
-  return (
-    <>
-      <h2 className="text-2xl font-semibold text-gray-800 mb-4 capitalize">
-        {category}
-      </h2>
-      {items.map((item) => (
-        <Link href={`/blog/${item.id}`} key={item.id}>
-          <div className="bg-white shadow-md rounded-lg p-6">
-            <h3 className="text-xl font-bold text-gray-900">{item.name}</h3>
-            <p className="text-gray-600 mt-2">{item.description}</p>
-            <p className="text-lg font-semibold text-gray-800 mt-4">
-              {item.price}
-            </p>
-          </div>
-        </Link>
-      ))}
-    </>
-  );
-}
-
-function getItem(itemId: string): MenuItem {
+function getAllMenuItems(): MenuItem[] {
   const allItems = Object.values(MENU_ITEMS).reduce((items, acc) => {
     return [...items, ...acc];
   }, []) as MenuItem[];
+  return allItems;
+}
+
+function getItem(itemId: string): MenuItem {
+  const allItems = getAllMenuItems();
   const item = allItems.find((item) => item.name === itemId);
   return item ?? MENU_ITEMS.appetizers[0];
+}
+
+// export const getStaticPaths = (async () => {
+//   const allPaths = getAllMenuItems().map((item) => {
+//     return {
+//       params: {
+//         name: item.name,
+//       },
+//     };
+//   });
+//   return {
+//     paths: allPaths,
+//     fallback: true, // false or "blocking"
+//   };
+// }) satisfies GetStaticPaths;
+
+// export const getInitialProps = (async (context) => {
+//   console.log('next context', context);
+//   const item = getItem(context.params.itemId);
+//   return { props: { item } }
+// }) satisfies GetStaticProps<{
+//   item: MenuItem
+// }>
+
+export async function generateStaticParams() {
+  const items = getAllMenuItems();
+
+  return items.map((item) => ({
+    itemId: item.name,
+  }));
 }
 
 export default function Item({ params }: { params: { itemId: string } }) {
   const { itemId } = params;
   const urlDecodedItemId = decodeURIComponent(itemId);
   const item = getItem(urlDecodedItemId);
-  console.log({ itemId });
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       <h1 className="text-4xl font-bold text-center mb-8">Menu Item</h1>
